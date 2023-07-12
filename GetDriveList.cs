20da90e47f3d9dbfdc331f127d9ba279a5eb1c87
Kyle Montoya <k.montoya87@outlook.com>
2023-07-12 14:07:58 -0700

    Add project files.

---
+++ GetDriveList.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+
+namespace WinVerMediaChecker
+{
+    internal class GetDriveList
+    {
+        #region Public Constructors
+
+        public GetDriveList(MainWindow? mainWindow)
+        {
+            _mainWindow = mainWindow;
+        }
+
+        #endregion
+
+        #region Private Fields
+
+        /// <summary>
+        /// Readonly <see cref="Dictionary{int, char}"/> containing all-lower characters of alphabet 'a' to 'z' using an index starting with '1'.
+        /// </summary>
+        private readonly Dictionary<int, char> _driveLetterList = new()
+        {
+            {1, 'a'},
+            {2, 'b'},
+            {3, 'c'},
+            {4, 'd'},
+            {5, 'e'},
+            {6, 'f'},
+            {7, 'g'},
+            {8, 'h'},
+            {9, 'i'},
+            {10, 'j'},
+            {11, 'k'},
+            {12, 'l'},
+            {13, 'm'},
+            {14, 'n'},
+            {15, 'o'},
+            {16, 'p'},
+            {17, 'q'},
+            {18, 'r'},
+            {19, 's'},
+            {20, 't'},
+            {21, 'u'},
+            {22, 'v'},
+            {23, 'w'},
+            {24, 'x'},
+            {25, 'y'},
+            {26, 'z'}
+        };
+
+        private readonly MainWindow? _mainWindow;
+
+        #endregion
+
+        #region Public Properties
+
+        public char[] ReadyDriveArray { get; set; }
+
+        #endregion
+
+        #region Public Methods
+
+        public Dictionary<int, string> Refresh()
+        {
+            var driveLetterIndex = Enumerable.Range(1, 26);
+            var refreshedDriveList = new Dictionary<int, string>();
+
+            int refreshedDriveCount = 1;
+
+            if (_mainWindow is not null)
+            {
+                foreach (var driveLetterKey in driveLetterIndex)
+                {
+                    var _driveInfo = new DriveInfo(_driveLetterList[driveLetterKey].ToString());
+                    if (_driveInfo.IsReady == true)
+                    {
+                        refreshedDriveList.Add(refreshedDriveCount++, _driveInfo.RootDirectory.ToString().ToUpper());
+                        if (_driveInfo.Name != _driveInfo.RootDirectory.ToString())
+                        {
+                            _mainWindow.availableDrivesDropDownMenu.Items.Add($"#{refreshedDriveCount++}).    {_driveInfo.RootDirectory.ToString().ToUpper()}   {_driveInfo.Name}");
+                            break;
+                        }
+                        _mainWindow.availableDrivesDropDownMenu.Items.Add($"#{refreshedDriveCount++}).    {_driveInfo.RootDirectory.ToString().ToUpper()}");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show($"MainWindow = {_mainWindow}");
+            }
+
+            return refreshedDriveList;
+        }
+
+        #endregion
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WinVerMediaChecker;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    #region Public Constructors
+
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    private void DriveRefresh()
+    {
+        availableDrivesDropDownMenu.Items.Clear();
+       _ = new GetDriveList(this).Refresh();
+
+
+
+
+
+    }
+
+    private void refreshDrivesButton_Click(object sender, RoutedEventArgs e)
+    {
+        DriveRefresh();
+    }
+
+    private void Window_Loaded(object sender, RoutedEventArgs e)
+    {
+        DriveRefresh();
+
+    }
+
+    #endregion
+
+    private void availableDrivesDropDownMenu_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+    {
+        driveSelectionConfirmation.Content = availableDrivesDropDownMenu.SelectedItem.ToString()!.Remove(0, 8);
+    }
+}
